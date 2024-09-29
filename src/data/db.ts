d97303d2573a2d43bbd2973a6daeae3d16c3fd86
Mauricio Erazo <antonierazo13@hotmail.com>
2024-09-29 04:18:28 +0200

    feat: created interfaces

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,4 +1,12 @@
-export const db = [
+export interface Guitar {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+}
+
+export const db: Guitar[] = [
   {
     id: 1,
     name: 'Lukather',
