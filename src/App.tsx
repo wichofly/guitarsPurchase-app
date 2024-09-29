a4064fceed3656a968867ed0bde518561a7b83a8
Mauricio Erazo <antonierazo13@hotmail.com>
2024-09-29 03:10:19 +0200

    feat: custom hook created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-function App() {
+import Guitar from './components/Guitar';
+import Header from './components/Header';
+import { useCart } from './hooks/useCart';
 
+function App() {
+  const {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    decreaseQuantity,
+    increaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal,
+  } = useCart();
 
   return (
     <>
-      <h1>Welcome to Guitars World!</h1>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        decreaseQuantity={decreaseQuantity}
+        increaseQuantity={increaseQuantity}
+        clearCart={clearCart}
+        isEmpty={isEmpty}
+        cartTotal={cartTotal}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Our Collection</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarPurchase - All rights reserved.{' '}
+          </p>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,121 @@
+import { PiGuitarLight } from 'react-icons/pi';
+
+// interface Props {
+//   cart: Cart[];
+//   removeFromCart: (id: string) => void;
+//   decreaseQuantity: (id: string) => void;
+//   increaseQuantity: (id: string) => void;
+//   clearCart: () => void;
+//   isEmpty: boolean;
+//   cartTotal: number;
+// }
+
+export default function Header({
+  cart,
+  removeFromCart,
+  decreaseQuantity,
+  increaseQuantity,
+  clearCart,
+  isEmpty,
+  cartTotal,
+}) {
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <h4>
+                Guitar Purchase
+                <PiGuitarLight fontSize="3rem" />
+                <br />
+              </h4>
+            </a>
+          </div>
+          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito">
+              <img
+                className="img-fluid"
+                src="/img/carrito.png"
+                alt="imagen carrito"
+              />
+
+              <div id="carrito" className="bg-white p-3">
+                {isEmpty ? (
+                  <p className="text-center">Cart is empty</p>
+                ) : (
+                  <>
+                    <table className="w-100 table">
+                      <thead>
+                        <tr>
+                          <th>Image</th>
+                          <th>Name</th>
+                          <th>Price</th>
+                          <th>Quantity</th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {cart.map((guitar) => (
+                          <tr key={guitar.id}>
+                            <td>
+                              <img
+                                className="img-fluid"
+                                src={`/img/${guitar.image}.jpg`}
+                                alt="imagen guitarra"
+                              />
+                            </td>
+                            <td>{guitar.name}</td>
+                            <td className="fw-bold">${guitar.price}</td>
+                            <td className="flex align-items-start gap-4">
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => decreaseQuantity(guitar.id)}
+                              >
+                                -
+                              </button>
+                              {guitar.quantity}
+                              <button
+                                type="button"
+                                className="btn btn-dark"
+                                onClick={() => increaseQuantity(guitar.id)}
+                              >
+                                +
+                              </button>
+                            </td>
+                            <td>
+                              <button
+                                className="btn btn-danger"
+                                type="button"
+                                onClick={() => removeFromCart(guitar.id)}
+                              >
+                                X
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+
+                    <p className="text-end">
+                      Total to pay:
+                      <span className="fw-bold"> ${cartTotal}</span>
+                    </p>
+                  </>
+                )}
+
+                <button
+                  className="btn btn-dark w-100 mt-3 p-2"
+                  onClick={clearCart}
+                >
+                  Empty Cart
+                </button>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
